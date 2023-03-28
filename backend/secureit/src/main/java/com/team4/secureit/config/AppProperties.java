10f0ab60f19e166a09c08e1f1cd8b23b0138f420
s4ndu <lazicdusan1104@gmail.com>
2023-03-28 20:21:51 +0200

    Add app configuration

---
+++ backend/secureit/src/main/java/com/team4/secureit/config/AppProperties.java
@@ -0,0 +1,30 @@
+package com.team4.secureit.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+    private final Auth auth = new Auth();
+    private final Mailing mailing = new Mailing();
+
+    @Getter
+    @Setter
+    public static class Auth {
+        private String tokenSecret;
+        private Integer tokenExpirationSeconds;
+    }
+
+    @Getter
+    @Setter
+    public static class Mailing {
+        private String templatesLocation;
+    }
+
+    public Auth getAuth() {
+        return auth;
+    }
+
+    public Mailing getMailing() { return mailing; }
+}
