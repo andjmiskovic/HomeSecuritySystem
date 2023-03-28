15c09d52b2b9c907e3834cdb997fc558177fa175
mmmajder <ajder.milan2000@gmail.com>
2023-03-28 22:29:50 +0200

    added sidebar and certificate list

--- frontend/secure-it/src/app/app.module.ts
+++ frontend/secure-it/src/app/app.module.ts
@@ -5,21 +5,37 @@ import {AppComponent} from './app.component';
 import {HomepageModule} from "./views/homepage/homepage.module";
 import {NotFoundPageComponent} from "./views/404/not-found-page/not-found-page.component";
 import {NotAuthorizedPageComponent} from "./views/403/not-authorized-page/not-authorized-page.component";
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import {MatButtonModule} from "@angular/material/button";
+import {CertificateListComponent} from './views/certificate-list/certificate-list.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatSelectModule} from "@angular/material/select";
+import {SharedModule} from './shared/shared.module'
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundPageComponent,
     NotAuthorizedPageComponent,
+    CertificateListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HomepageModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    SharedModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
