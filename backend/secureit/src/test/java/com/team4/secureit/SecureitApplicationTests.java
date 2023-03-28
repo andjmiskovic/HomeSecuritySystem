d2e30c2e007ebe7a2c3e84a8da1e0d507f88c49b
s4ndu <lazicdusan1104@gmail.com>
2023-03-28 19:44:51 +0200

    Initialize Spring Boot project

---
+++ backend/secureit/src/test/java/com/team4/secureit/SecureitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.secureit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
