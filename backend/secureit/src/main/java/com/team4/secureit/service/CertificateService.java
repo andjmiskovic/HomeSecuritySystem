a36eefff3c4a33e5c570eb4b9cf351bf751f8f90
Dušan Lazić <lazicdusan1104@gmail.com>
2023-03-30 08:38:47 +0200

    Badly implement extensions

--- backend/secureit/src/main/java/com/team4/secureit/service/CertificateService.java
+++ backend/secureit/src/main/java/com/team4/secureit/service/CertificateService.java
@@ -3,8 +3,11 @@ package com.team4.secureit.service;
 import com.team4.secureit.dto.request.CertificateCreationOptions;
 import com.team4.secureit.model.CertificateDetails;
 import com.team4.secureit.repository.CertificateDetailsRepository;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.CertIOException;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
@@ -23,6 +26,7 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class CertificateService {
@@ -41,26 +45,36 @@ public class CertificateService {
         return certificateDetailsRepository.findIssuerCertificates();
     }
 
-    public X509Certificate generateCertificate(PKCS10CertificationRequest csr, CertificateCreationOptions options) throws KeyStoreException, CertificateException, OperatorCreationException {
+    public X509Certificate generateCertificate(PKCS10CertificationRequest csr, CertificateCreationOptions options) throws KeyStoreException, CertificateException, OperatorCreationException, CertIOException {
         SubjectPublicKeyInfo subjectPublicKeyInfo = csr.getSubjectPublicKeyInfo();
         X500Name subjectX500Name = csr.getSubject();
+        Map<String, String> extensions = options.getExtensions();
 
         String issuerAlias = options.getIssuerAlias();
         PrivateKey issuerPrivateKey = keyStoreService.getKeyPair(issuerAlias, "privatekeypassword".toCharArray()).getPrivate();
         X500Name issuerX500Name = new JcaX509CertificateHolder(keyStoreService.getCertificate(issuerAlias)).getSubject();
 
-        BigInteger serialNumber = new BigInteger(128, new SecureRandom());
+        BigInteger serialNumber = new BigInteger(64, new SecureRandom());
         Date notBefore = new Date();
         Date notAfter = new Date(System.currentTimeMillis() + 365L * 24L * 60L * 60L * 1000L);
 
-        X509CertificateHolder certHolder = new X509v3CertificateBuilder(
+        X509v3CertificateBuilder certBuilder = new X509v3CertificateBuilder(
                 issuerX500Name,
                 serialNumber,
                 notBefore,
                 notAfter,
                 subjectX500Name,
-                subjectPublicKeyInfo)
-                .build(new JcaContentSignerBuilder("SHA256WithRSA").build(issuerPrivateKey));
+                subjectPublicKeyInfo);
+
+        // TODO: Fix implementation, this one does not follow X509
+        for (Map.Entry<String, String> entry : extensions.entrySet()) {
+            String objectId = entry.getKey();
+            String value = entry.getValue();
+
+            certBuilder.addExtension(new ASN1ObjectIdentifier(objectId), false, new DEROctetString(value.getBytes()));
+        }
+
+        X509CertificateHolder certHolder = certBuilder.build(new JcaContentSignerBuilder("SHA256WithRSA").build(issuerPrivateKey));
 
         return new JcaX509CertificateConverter().getCertificate(certHolder);
     }
