27a93a1573fa6cd78e1d0c2a954fe1025180b57c
mmmajder <ajder.milan2000@gmail.com>
2023-05-04 18:31:35 +0200

    changed visibility of possesion of property

--- frontend/secure-it/src/app/views/objects/components/property-card/property-card.component.ts
+++ frontend/secure-it/src/app/views/objects/components/property-card/property-card.component.ts
@@ -13,8 +13,10 @@ export class PropertyCardComponent implements OnInit {
 
   @Input() property!: BasicPropertyDetails;
   loggedUserId!: number
+  userRole!: string
 
   constructor(private dialog: MatDialog, private authService: AuthService) {
+    this.userRole = localStorage.getItem("userRole") || ""
   }
 
   ngOnInit() {
