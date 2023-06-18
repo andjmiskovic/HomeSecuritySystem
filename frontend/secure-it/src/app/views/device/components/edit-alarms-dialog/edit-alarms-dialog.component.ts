ceeaae76aa14ca6dfb2989918f52b3205075a513
Anđela <andjelamiskovic2000@gmail.com>
2023-06-18 14:28:09 +0200

    device page changes

---
+++ frontend/secure-it/src/app/views/device/components/edit-alarms-dialog/edit-alarms-dialog.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {DeviceManagementService} from "../../../../services/device.service";
+import {DeviceDetailsResponse} from "../../../../model/Device";
+
+@Component({
+  selector: 'app-edit-alarms-dialog',
+  templateUrl: './edit-alarms-dialog.component.html',
+  styleUrls: ['./edit-alarms-dialog.component.css']
+})
+export class EditAlarmsDialogComponent implements OnInit {
+  @Input() device: DeviceDetailsResponse = new DeviceDetailsResponse();
+
+  constructor(private deviceService: DeviceManagementService) {
+  }
+
+  ngOnInit() {
+  }
+
+}
